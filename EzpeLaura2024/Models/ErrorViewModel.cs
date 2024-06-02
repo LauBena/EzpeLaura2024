adb549a8bf2897061490a1bcadf9245485e2735e
LauBena <laurabr314@gmail.com>
2024-06-02 17:13:25 -0300

    creacion del proyecto

---
+++ EzpeLaura2024/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EzpeLaura2024.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
