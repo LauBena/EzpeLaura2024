181cfcb03e0a9cd9a2a43d3a7fcc3d524d197aa2
LauBena <laurabr314@gmail.com>
2024-06-03 16:19:57 -0300

    midificaciones en el modelo tipo de ejercicio

--- EzpeLaura2024/Migrations/20240603184559_tablaTipoEjercicio.Designer.cs
+++ EzpeLaura2024/Migrations/20240603184559_tablaTipoEjercicio.Designer.cs
@@ -4,6 +4,7 @@ using EzpeLaura2024.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EzpeLaura2024.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240603184559_tablaTipoEjercicio")]
+    partial class tablaTipoEjercicio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
