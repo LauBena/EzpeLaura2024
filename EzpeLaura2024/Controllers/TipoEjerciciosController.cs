924081b0fc343cced6bbf92e9eb1b3fa0eb25c54
LauBena <laurabr314@gmail.com>
2024-06-03 18:49:06 -0300

    Controlador tipo ejercicios listo

--- EzpeLaura2024/Controllers/TipoEjerciciosController.cs
+++ EzpeLaura2024/Controllers/TipoEjerciciosController.cs
@@ -38,5 +38,90 @@ public class TipoEjerciciosController : Controller
         return Json(tipoDeEjercicios);
     }
 
+    public JsonResult GuardarTipoEjercicio(int tipoEjercicioID, string descripcion)
+    {
+        //1- VERIFICAMOS SI INGRESO ALGUN CARACTER Y LA VARIABLE NO SEA NULL
+        // if(descripcion != null && descripcion != "")
+        // {
+        //      INGRESA SI ESCRIBE SI O SI    
+        //}
 
+        //if(String.IsNullOrEmpty(descripcion)== false)
+        //{
+        // INGRESA SI ESCRIBE SI O SI
+        //}
+
+        string resultado = "";
+
+        if (!String.IsNullOrEmpty(descripcion))
+        {
+            descripcion = descripcion.ToUpper();
+            //INGRESA SI ESCRIBIO SI O SI 
+
+            //2-VERIFICA SI EDITA O CREA NUEVO REGISTRO
+            if (tipoEjercicioID == 0)
+            {
+                //3-VERIFICAMOS SI EXISTE EN BASE DE DATOS UN REGISTRO CON LA MISMA DESCRIPCION
+                //BUSCAMOS EN EL CONTEXTO (BASE DE DATOS)
+                //SI EXISTE UN REGISTRO CON ESA DESCRIPCION
+                var existeTipoEjercicio = _context.TipoEjercicios.Where(t => t.Descripcion == descripcion).Count();
+                if (existeTipoEjercicio == 0)
+                {
+                    //4-GUARDA EL TIPO DE EJERCICIO
+                    var tipoEjercicio = new TipoEjercicio
+                    {
+                        Descripcion = descripcion,
+                    };
+                    _context.Add(tipoEjercicio);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    resultado = "YA EXISTE UN REGISTRO CON LA MISMA DESCRIPCION";
+                }
+            }
+            else
+            {
+                //VAMOS A EDITAR EL REGISTRO
+                var tipoEjercicioEditar = _context.TipoEjercicios.Where(t => t.TipoEjercicioID == tipoEjercicioID).SingleOrDefault();
+                if (tipoEjercicioEditar != null)
+                {
+                    //BUSCAMOS EN TABLA SI EXISTE UN REGISTRO CON MISMO NOMBRE, PERO DISTINTO ID AL QUE ESTAMOS EDITANDO
+                    var existeTipoEjercicio = _context.TipoEjercicios.Where(t => t.Descripcion == descripcion && t.TipoEjercicioID != tipoEjercicioID).Count();
+                    if (existeTipoEjercicio == 0)
+                    {
+                        //QUIERE DECIR QUE EXISTE Y ES CORRECTO, ENTONCES CONTINUAMOS EDITANDO
+                        tipoEjercicioEditar.Descripcion = descripcion;
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        resultado = "YA EXISTE UN REGISTRO CON LA MISMA DESCRIPCION";
+                    }
+                }
+            }
+        }
+        else
+        {
+            resultado = "DEBE INGRESAR UNA DESCRIPCION";
+        }
+        return Json(resultado);
+    }
+
+    public JsonResult EliminarTipoEjercicio(int tipoEjercicioID)
+    {
+        bool eliminado = false;
+
+        //BUSCO SI EXISTEN EJERCICIOS CARGADOS
+        var existeEjercicio = _context.EjercicioFisicos.Where(t => t.TipoEjercicioID == tipoEjercicioID).Count();
+        if (existeEjercicio == 0)
+        {
+            var tipoEjercicio = _context.TipoEjercicios.Find(tipoEjercicioID);
+            _context.Remove(tipoEjercicio);
+            _context.SaveChanges();
+            eliminado = true;
+        }
+
+        return Json(eliminado);
+    }
 }
